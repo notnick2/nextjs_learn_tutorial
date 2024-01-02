0bfee93ce0f46f3d368ae710f51764a2077eeb76
notnick2 <varun024123@gmail.com>
2024-01-01 23:48:28 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,9 @@ export default function Page() {
           {/* Add Hero Images Here */}
         </div>
       </div>
+      <div
+  className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
+/>
     </main>
   );
 }
